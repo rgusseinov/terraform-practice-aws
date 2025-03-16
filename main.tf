89018dbebf3d89c1c97416bd6c21e9d825536693
Ruslan Gusseinov <r.gusseinov@mail.ru>
2025-03-16 14:24:30 +0500

    create terraform infrastructure with nginx installation

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
+provider "aws" {
+  region = var.region
+}
+
 terraform {
   required_version = ">= 1.5"
   backend "s3" {}
-}
-
-provider "aws" {
-  region = var.aws_region
 }
 
 resource "aws_vpc" "my_vpc" {
@@ -38,12 +38,23 @@ resource "aws_instance" "foo" {
   ami = var.ami_id
   instance_type = var.instance_type
   subnet_id = aws_subnet.my_subnet.id
+  associate_public_ip_address = true
 
   vpc_security_group_ids = [aws_security_group.ssh.id]
+  
+  key_name = "my-new-key"
 
   credit_specification {
     cpu_credits = "unlimited"
   }
+
+
+  user_data = <<-EOF
+                  #!/bin/bash
+                  sudo dnf install -y nginx
+                  sudo systemctl enable nginx
+                  sudo systemctl start nginx
+                  EOF
 
   tags = {
     Name = "HelloWorld"
@@ -62,7 +73,57 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "AllowSSH"
   }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "terraform-state-124578"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.route_table.id
 }
